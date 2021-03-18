68cbcfb36399d0a43b46c6948eec5d314b112245
Mikk-Markus Soomre <mmsoomre@pm.me>
2021-03-18 11:17:48 +0200

    tests for variety of generics

--- src/test/java/jvm2dts/ConverterTest.java
+++ src/test/java/jvm2dts/ConverterTest.java
@@ -2,10 +2,7 @@ package jvm2dts;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,7 +17,9 @@ public class ConverterTest {
       "ids: string[]; " +
       "map: {[key: string]: number}; " +
       "mapInMap: {[key: string]: {[key: string]: number}}; " +
-      "generic: Generic<T,U,V>;}");
+      "generic: Generic<T,U,V>; " +
+      "extendedGeneric: ExtendedGeneric<T>; " +
+      "rawType: ArrayList;}");
   }
 
   enum Role {
@@ -34,10 +33,13 @@ public class ConverterTest {
     Map<String, Integer> map;
     Map<String, Map<String, Integer>> mapInMap;
     Generic<?, ?, ?> generic;
+    ExtendedGeneric<? extends Role> extendedGeneric;
+    ArrayList rawType;
 
     static boolean doNotGenerate = true;
     static Date unwantedDate = new Date();
   }
 
   static class Generic<T, U, V> {}
+  static class ExtendedGeneric<T> {}
 }
