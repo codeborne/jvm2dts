465a9708113ced5f00c0516d4b22dca80a9f766b
Anton Keks <anton@azib.net>
2025-06-26 10:54:26 +0300

    iterate not packages (there is a bug with recursive handling), but classes themselves

--- src/main/java/jvm2dts/Main.java
+++ src/main/java/jvm2dts/Main.java
@@ -3,21 +3,15 @@ package jvm2dts;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import static java.lang.System.*;
+import static java.lang.System.err;
+import static java.lang.System.out;
 import static java.util.Arrays.stream;
 import static java.util.Collections.emptySet;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 public class Main {
@@ -57,24 +51,12 @@ public class Main {
       return;
     }
 
-    var packages = parsedArgs.packages;
+    var packages = new LinkedHashSet<>(parsedArgs.packages);
     var dataOnly = parsedArgs.dataOnly;
     var withAnnotations = parsedArgs.withAnnotations != null ? stream(parsedArgs.withAnnotations.split(",")).collect(toSet()) : null;
     var basePath = Paths.get(parsedArgs.classesDir);
     Set<String> excludeDirs = parsedArgs.excludeDirs == null ? emptySet() :
       stream(parsedArgs.excludeDirs.split(",")).collect(toSet());
-
-    var classLoader = Main.class.getClassLoader();
-    if (packages.isEmpty() && parsedArgs.classesDir != null) {
-      packages = findPackages(basePath, excludeDirs);
-      err.println("Packages detected: " + packages);
-    }
-
-    if (packages.isEmpty()) {
-      err.println("No packages found");
-      jc.usage();
-      exit(1);
-    }
 
     var customTypes = new HashMap<Class<?>, String>();
     if (parsedArgs.cast != null) {
@@ -85,62 +67,38 @@ public class Main {
     }
     var converter = new Converter(new TypeMapper(customTypes));
 
-    for (var packageName : packages) {
-      var packageUrl = classLoader.getResource(packageName.replace('.', '/'));
-      if (packageUrl == null) {
-        err.println("Cannot load " + packageName + " using ClassLoader, is it missing from classpath?");
-        continue;
-      }
-
-      if (packageUrl.getProtocol().equals("file")) {
-        try {
-          var exclude = parsedArgs.excludeRegex;
-          Files.walk(Paths.get(packageUrl.toURI())).filter(Files::isRegularFile).filter(path -> {
-            var pathString = path.getFileName().toString();
-            if (pathString.endsWith(".class")) {
-              if (exclude == null) return true;
-              var classString = pathString.substring(0, pathString.lastIndexOf('.'));
-              return !classString.matches(exclude);
-            }
-            return false;
-          })
-          .map(path -> packageName + "." + path.getFileName().toString().substring(0, path.getFileName().toString().lastIndexOf('.')))
-          .sorted().forEach(className -> {
-            try {
-              Class<?> clazz = Class.forName(className);
-              if ((dataOnly && !isData(clazz) || withAnnotations != null && !isAnnotated(clazz, withAnnotations)) &&
-                  !clazz.isEnum() && !clazz.isInterface()) return;
-              // TODO: record support
-              var converted = converter.convert(clazz);
-              if (converted != null) {
-                out.print("// ");
-                out.println(className);
-                out.print("export ");
-                out.println(converted);
-              }
-            } catch (Throwable e) {
-              err.println("// Failed to load: " + e);
-            }
-          });
-        } catch (IOException | URISyntaxException e) {
-          err.println("// Could not access package " + packageName + ": " + e);
+    try {
+      var exclude = parsedArgs.excludeRegex;
+      Files.walk(basePath).filter(Files::isRegularFile).filter(path -> {
+        var pathString = path.getFileName().toString();
+        if (pathString.endsWith(".class")) {
+          if (exclude == null) return true;
+          var classString = pathString.substring(0, pathString.lastIndexOf('.'));
+          return !classString.matches(exclude);
         }
-      } else {
-        err.println("// Cannot load " + packageUrl + ": unsupported protocol");
-        exit(3);
-      }
+        return false;
+      })
+    .map(path -> path.toString().substring(basePath.toString().length() + 1).replace(".class", "").replace("/", "."))
+      .filter(className -> packages.isEmpty() || packages.contains(className.substring(0, className.lastIndexOf('.'))))
+      .sorted().forEach(className -> {
+        try {
+          Class<?> clazz = Class.forName(className);
+          if ((dataOnly && !isData(clazz) || withAnnotations != null && !isAnnotated(clazz, withAnnotations)) &&
+              !clazz.isEnum() && !clazz.isInterface()) return;
+          var converted = converter.convert(clazz);
+          if (converted != null) {
+            out.print("// ");
+            out.println(className);
+            out.print("export ");
+            out.println(converted);
+          }
+        } catch (Throwable e) {
+          err.println("// Failed to load: " + e);
+        }
+      });
+    } catch (IOException e) {
+      err.println("// Could not access classes in " + basePath + ": " + e);
     }
-  }
-
-  private static List<String> findPackages(Path basePath, Set<String> excludeDirs) throws IOException {
-    return Files.walk(basePath)
-      .filter(Files::isDirectory)
-      .filter(path -> !path.getFileName().toString().equals("META-INF"))
-      .filter(path -> !path.equals(basePath))
-      .filter(path -> !excludeDirs.contains(basePath.relativize(path).toString()))
-      .sorted()
-      .map(name -> basePath.relativize(name).toString().replace(File.separatorChar, '.'))
-      .collect(toList());
   }
 
   private static boolean isData(Class<?> clazz) {
